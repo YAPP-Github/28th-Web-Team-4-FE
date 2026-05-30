46d09935f055de7a1d295fb07f8e4b1933731745
DongjaJ <jdh981118@naver.com>
2026-05-30 14:50:20 +0900

    chore: 빌드 시 console 제거

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  ...(process.env.NEXT_PUBLIC_NODE_ENV === "prod" && {
+    compiler: {
+      removeConsole: {
+        exclude: ["error"],
+      },
+    },
+  }),
 };
 
 export default nextConfig;
