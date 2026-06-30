00489ca978936785f4854669495fe155ca6ce227
Donghwan Jeung <jdh981118@naver.com>
2026-06-30 19:30:00 +0900

    feat(ui): add shared layout primitives and format button story

---
+++ src/shared/ui/layout/h-stack.tsx
@@ -0,0 +1,14 @@
+import type { JSX, ElementType, PropsWithChildren } from 'react';
+
+import { type BoxProps, Box } from './box';
+
+export const HStack: <C extends ElementType = 'div'>(
+  props: PropsWithChildren<BoxProps<C>>,
+) => JSX.Element = <C extends ElementType = 'div'>({
+  className,
+  ...rest
+}: PropsWithChildren<BoxProps<C>>) => {
+  const typesRest = rest as BoxProps<C>;
+
+  return <Box className={`flex items-center ${className}`} {...typesRest} />;
+};
---
+++ src/shared/ui/layout/justify-end.tsx
@@ -0,0 +1,14 @@
+import type { JSX, ElementType, PropsWithChildren } from 'react';
+
+import { type BoxProps, Box } from './box';
+
+export const JustifyEnd: <C extends ElementType = 'div'>(
+  props: PropsWithChildren<BoxProps<C>>,
+) => JSX.Element = <C extends ElementType = 'div'>({
+  className,
+  ...rest
+}: PropsWithChildren<BoxProps<C>>) => {
+  const typesRest = rest as BoxProps<C>;
+
+  return <Box className={`flex justify-end ${className}`} {...typesRest} />;
+};
