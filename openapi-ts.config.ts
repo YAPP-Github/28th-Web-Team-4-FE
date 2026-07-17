3c33493e456c96a48fc7735eb72542908d9a4c41
yereong <lily030624@gmail.com>
2026-07-17 15:31:28 +0900

    chore: 백엔드 Docs url 변경

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -7,7 +7,7 @@ const toKebabCase = (name: string) =>
     .toLowerCase();
 
 export default defineConfig({
-  input: 'http://43.200.117.165/v3/api-docs',
+  input: 'https://api.chaeso-zip.com/v3/api-docs',
   output: {
     path: 'src/shared/api/generated',
     fileName: {
