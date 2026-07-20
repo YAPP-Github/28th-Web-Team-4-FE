731840d5a04a98c975d27725890317d22851a305
DongjaJ <jdh981118@naver.com>
2026-07-21 00:08:43 +0900

    chore: Storybook coverage Codecov 업로드 추가

--- vitest.config.ts
+++ vitest.config.ts
@@ -33,6 +33,9 @@ export default defineConfig({
     projects: [
       {
         extends: true,
+        optimizeDeps: {
+          include: ['@base-ui/react/input'],
+        },
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
