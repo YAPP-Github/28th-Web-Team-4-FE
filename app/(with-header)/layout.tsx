5be4431fa6134183e006b2c9dda1f4bad74bf220
DongjaJ <jdh981118@naver.com>
2026-08-05 23:52:37 +0900

    fix: 가변 헤더 높이에 맞춰 페이지 높이를 계산한다.

--- app/(with-header)/layout.tsx
+++ app/(with-header)/layout.tsx
@@ -19,13 +19,11 @@ export default async function WithHeaderLayout({ children }: { children: ReactNo
   queryClient.setQueryData(authSessionQueryKey, sessionState);
 
   return (
-    <>
-      <div className="shrink-0">
-        <HydrationBoundary state={dehydrate(queryClient)}>
-          <SessionPageHeader />
-        </HydrationBoundary>
-      </div>
-      {children}
-    </>
+    <div className="flex h-dvh min-h-0 flex-none flex-col">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <SessionPageHeader />
+      </HydrationBoundary>
+      <div className="flex min-h-0 flex-1 flex-col">{children}</div>
+    </div>
   );
 }
