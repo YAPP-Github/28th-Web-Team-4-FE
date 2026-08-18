d27193f3d6c8b2aca551b6fa9e1babc6d59d55ad
DongjaJ <jdh981118@naver.com>
2026-08-19 00:17:28 +0900

    feat: 푸터 디자인을 피그마 기준으로 수정

--- src/pages/home/ui/footer.test.tsx
+++ src/pages/home/ui/footer.test.tsx
@@ -8,12 +8,18 @@ describe('Footer', () => {
 
     expect(screen.getByRole('contentinfo')).toBeInTheDocument();
     expect(screen.getByRole('img', { name: 'chaesozip' })).toBeInTheDocument();
-    expect(screen.getByText('내게 맞는 광고 채널을 한눈에! 채소집')).toBeInTheDocument();
+    expect(screen.getByText('© 2026 CHAESOZIP. ALL RIGHTS RESERVED')).toBeInTheDocument();
     expect(screen.getByText('이용 약관')).toBeInTheDocument();
-    expect(screen.getByText('개인정보 처리 방침')).toBeInTheDocument();
-    expect(screen.getByText('블로그')).toBeInTheDocument();
+    expect(screen.getByText('개인정보 처리방침')).toBeInTheDocument();
     expect(screen.getByText('요금제')).toBeInTheDocument();
-    expect(screen.getByText('문의 : channelsogae.zip@gmail.com')).toBeInTheDocument();
-    expect(screen.getByText('2026 Team Chaesozip. All right reservation')).toBeInTheDocument();
+    expect(screen.getByRole('img', { name: '이메일' })).toBeInTheDocument();
+    expect(screen.getByRole('img', { name: '네이버 블로그' })).toBeInTheDocument();
+
+    expect(screen.queryByText('내게 맞는 광고 채널을 한눈에! 채소집')).not.toBeInTheDocument();
+    expect(screen.queryByText('블로그')).not.toBeInTheDocument();
+    expect(screen.queryByText('문의 : channelsogae.zip@gmail.com')).not.toBeInTheDocument();
+    expect(
+      screen.queryByText('2026 Team Chaesozip. All right reservation'),
+    ).not.toBeInTheDocument();
   });
 });
